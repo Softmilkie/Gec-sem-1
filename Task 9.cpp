ff73dc162bc80442af050cb0ed7a297f13d4e43c
Louis <92083608+Softmilkie@users.noreply.github.com>
2022-08-07 20:03:24 +0100

    Create Task 9.cpp

---
+++ Task 9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	for (int i = 0; i <= 3; i++)
+	{
+		if (i % 2 == 0)
+			cout << i << "is even\n";
+		else
+			cout << i << " is odd\n";
+
+	}
+	return 0
+}
